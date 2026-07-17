24c8595d5e05d50f6d1a85593b1f6dd29854edbb
vishalbhardwaj0015 <vishalbhardwaj5556015@gmail.com>
2026-07-17 11:48:27 +0530

    fix tiDB SSL connection

--- server/config/db.js
+++ server/config/db.js
@@ -7,9 +7,11 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME || "sys",
+  port: parseInt(process.env.DB_PORT || "4000"),
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: { minVersion: "TLSv1.2", rejectUnauthorized: false },
 });
 
 export default pool;
